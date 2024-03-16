a04cda041bd38dd57a0ed059ba6013ce9d525464
qntnitaa <qonitaaf04@gmail.com>
2024-03-16 17:03:57 +0700

    File tugas 3 JavaScript

--- js/skrip.js
+++ js/skrip.js
@@ -2,7 +2,7 @@ function bio(){
     let nama = document.getElementById('frm').nama.value;
     let pekerjaan = document.getElementById('frm').pekerjaan.value;
     let hobby = document.getElementById('frm').hobby.value;
-    let input = 'Halo ' + nama + ' Biodata kamu : ' + '<br> nama ' + nama + '<br> pekerjaan : ' + pekerjaan + '<br> hobby : ' + hobby;
+    let input = ' Biodata kamu : ' + '<br> nama ' + nama + '<br> pekerjaan : ' + pekerjaan + '<br> hobby : ' + hobby;
     let no_input = 'Maaf Nama atau kelas anda belum terisi';
     let hasil = (nama&&pekerjaan&&hobby !='') ? input : no_input;
     document.getElementById('hasil').innerHTML = hasil;
